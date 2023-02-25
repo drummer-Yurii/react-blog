512c2a1c53a6e15057a0d50fcb312518c56029ea
Yurii Rybachok <drummer2009@ukr.net>
2023-02-25 12:53:22 +0200

    upload file to server

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import axios from 'axios';
 
 const Write = () => {
   const [value, setValue] = useState('');
@@ -8,8 +9,25 @@ const Write = () => {
   const [file, setFile] = useState(null);
   const [cat, setCat] = useState("");
 
+  const upload = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+      const res = await axios.post("/upload", formData);
+      return res.data;
+    }catch(err) {
+      console.log(err);
+    }
+  }
+
   const handleClick = async (e) => {
     e.preventDefault()
+    const imgUrl = upload();
+    try {
+
+    }catch(err) {
+      console.log(err);
+    }
   }
   
   return (
